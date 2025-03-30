a29e59f38e7620d15d60fff4a07ab677a1c27db0
Reza-Gohari <112699945+Reza-Gohari@users.noreply.github.com>
2025-03-30 19:44:42 +0200

    change
    Chanche

--- Uke1/obj/Debug/net9.0/Uke1.AssemblyInfo.cs
+++ Uke1/obj/Debug/net9.0/Uke1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Uke1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a853346ebc08d5c01cab1a323e4ffe3f51be4b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Uke1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Uke1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
